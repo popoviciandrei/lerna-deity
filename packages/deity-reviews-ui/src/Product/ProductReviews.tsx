3b7f6746c98b3797206e0b777f429f5ccc1df21b
Andrei Popovici <me@andreipopovici.co.uk>
2020-01-27 01:19:57 +0000

    Added skeleton of deity-reviews-ui

---
+++ packages/deity-reviews-ui/src/Product/ProductReviews.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export type ProductReviewsType = {};
+
+export const ProductReviews: React.SFC<ProductReviewsType> = () => (<p>Reviews</p>);
+
+ProductReviews.propTypes = {};
